2288cedeb5a9f1b2b1e7d0988ba317dab48f0af5
kailsonalves <106539606+kailsonalves@users.noreply.github.com>
2024-03-04 08:19:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
 
+for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
 for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove("ativo");
 textos[j].classList.remove("ativo");
 }
@@ -11,5 +11,8 @@ textos[i].classList.add("ativo");
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-contadores[0].textContent = tempoObjetivo1;
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+
