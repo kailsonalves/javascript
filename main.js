bba31e8b554ffdfc245efd612819c7cfc5531a42
kailsonalves <106539606+kailsonalves@users.noreply.github.com>
2024-04-09 09:07:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -38,13 +38,19 @@ if (tempoFinal > 0){
 
 function atualizaCronometro (){
 for (let i = 0; i < contadores.length; i++) {
-
+document.getElementById("dias"+i).textContent =calculaTempo(tempos[i])[0];
+document.getElementById("horas"+i).textContent =calculaTempo(tempos[i])[1];
+document.getElementById("min"+i).textContent =calculaTempo(tempos[i])[2];
+document.getElementById("seg"+i).textContent =calculaTempo(tempos[i])[3];
 }
 }
 
-function comecaCronometro (){
+function comecaCronometro() {
     atualizaCronometro();
-    setInterval(atualizaCronometro,1000);}
-    // comecaConometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+ comecaCronometro();
+
 
     
