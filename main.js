cf58fd9b14a01be9e4a7fe9526f1558341a94c33
kailsonalves <106539606+kailsonalves@users.noreply.github.com>
2024-05-07 09:51:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
-    
+
 for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove("ativo");
 textos[j].classList.remove("ativo");
 }
@@ -16,8 +16,10 @@ const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo5 = new Date("2025-08-05T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4, tempoObjetivo5];
 
 function calculaTempo(tempoObjetivo) {
 let tempoAtual = new Date();
